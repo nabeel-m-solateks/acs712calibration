ab889e83a7a278d34347cd33cb7cc4108f5afff1
NABEEL004 <66759134+NABEEL004@users.noreply.github.com>
2023-08-29 12:07:40 +0800

    Adding C++ code

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "ACS712.h"
+
+#define CURRENT_READ 34
+
+ACS712  ACS(CURRENT_READ, 3.3, 4095, 10);
+
+
+void setup() {
+  Serial.begin(9600);
+  ACS.autoMidPoint();
+
+}
+
+void loop() {
+  float average = 0;
+  uint32_t start = millis();
+  for (int i = 0; i < 100; i++)
+  {
+    //  select sppropriate function
+    //  average += ACS.mA_AC_sampling();
+    average += ACS.mA_AC(50);
+  }
+  float mA = average / 100.0;
+  uint32_t duration = millis() - start;
+  Serial.print("Time: ");
+  Serial.print(duration);
+  Serial.print("  mA: ");
+  Serial.println(mA);
+
+  delay(1000);
+}
+
